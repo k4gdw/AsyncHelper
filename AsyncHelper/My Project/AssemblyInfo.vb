8ee5df79b4bde7707e96bf1aed0c8d4a3f84000b
Bryan Johns <bryan.johns@mh.alabama.gov>
2011-12-15 10:15:04 -0600

    got the msbuild and nuget package creation working

--- AsyncHelper/My Project/AssemblyInfo.vb
+++ AsyncHelper/My Project/AssemblyInfo.vb
@@ -1,34 +1,4 @@
-﻿Imports System.Reflection
-Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("K4GDW.Threading.AsyncHelper")> 
-<Assembly: AssemblyDescription("A class libary to handle asynchronous fire and forget.")> 
-<Assembly: AssemblyCompany("K4GDW")> 
-<Assembly: AssemblyProduct("K4GDW.Threading.AsyncHelper")> 
-<Assembly: AssemblyCopyright("Copyright © K4GDW 2011")> 
-<Assembly: AssemblyTrademark("K4GDW")> 
-
-<Assembly: ComVisible(False)>
+﻿Imports System.Runtime.InteropServices
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("83492c31-9a7d-4627-aba3-0618ecc7df72")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.1.1")> 
-<Assembly: AssemblyFileVersion("1.1.1")> 
---
+++ SolutionInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("K4GDW.Threading.AsyncHelper")> 
+<Assembly: AssemblyDescription("A class libary to handle asynchronous fire and forget.")> 
+<Assembly: AssemblyCompany("K4GDW")> 
+<Assembly: AssemblyProduct("K4GDW.Threading.AsyncHelper")> 
+<Assembly: AssemblyCopyright("Copyright © K4GDW 2011")> 
+<Assembly: AssemblyTrademark("K4GDW")> 
+<Assembly: ComVisible(False)>
+<Assembly: AssemblyVersion("1.1.*")> 
+<Assembly: AssemblyFileVersion("1.1.1")> 
